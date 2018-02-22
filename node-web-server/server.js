f53203a0aa76daa48d610b7a58cc85c2fcca2525
kevins <soun042564@gmail.com>
2018-02-22 15:44:25 -0500

    Section 5 Chapters 40 - 45

---
+++ node-web-server/server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const hbs = require('hbs'); //templating library Handlebars
+const fs = require('fs');
+
+var app = express();
+
+// add support for HBS partials
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+// How you register Middleware
+// Middleware does not move on unless next() is called
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+// Maintenance Middleware
+/*
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});*/
+
+// Middleware to help serve files in public folder
+app.use(express.static(__dirname + '/public'));
+
+// Can invoke methods from within HBS files using a Helper
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+// HTTP Route Handler
+
+app.get('/', (req, res) => {
+    //res.send('<h1>Hello Express!</h1>');
+    /*
+    res.send({
+        name: 'Poop',
+        likes: [
+            'biking',
+            'comics'
+        ]
+    });*/
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to the Home Page!'
+    })
+});
+
+app.get('/about', (req, res) => {
+    //res.send('About Page');
+    // render any templates you have defined the node view engine to use
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: "Unable to handle request"
+    });
+});
+
+// Bind the app to a port on your machine
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
